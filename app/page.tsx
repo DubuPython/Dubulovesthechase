07d3bc06edd0434ec496e2b108508bfead30777c
Dubu <jethroberboso25@gmail.com>
2026-09-02 11:28:17 +0800

    added monthsary celebration

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,56 @@
 import Navbar from '../components/Layout/Navbar';
 import HeroVideo from '../components/Sections/HeroVideo';
+import RecordHolder from '../components/Sections/RecordHolder';
+import Foryoupics from '../components/Sections/Foryoupics';
 import BulletinBoard from '../components/Sections/BulletinBoard';
 import PolaroidGallery from '../components/Sections/PolaroidGallery';
 import VoiceSnippets from '../components/Sections/VoiceSnippets';
 import MovieReviews from '../components/Sections/MovieReviews';
 import LoveLetters from '../components/Sections/LoveLetters';
 import FishbowlMessages from '../components/Sections/FishbowlMessages';
+import VirtualPet from '../components/Sections/VirtualPet';
 
 import UpdateLog from '../components/UI/Updatelog';
 import FloatingMusicPlayer from '../components/UI/FloatingMusicPlayer';
 import CornerCompanions from '../components/UI/CornerCompanions';
+import FloatingHearts from '../components/UI/FloatingHearts';
+import LoadingScreen from '../components/UI/LoadingScreen';
+import TulipEasterEgg from '../components/UI/TulipEasterEgg';
+import ComebackButton from '../components/UI/ComebackButton';
 
 export default function Home() {
   return (
     <main className="relative w-full min-h-screen bg-slate-50 dark:bg-[#0f0f1b] overflow-x-hidden selection:bg-pink-300 selection:text-pink-900">
       
-      {/* Popups and UI Overlays */}
+      {/* Loaders and Full-Screen Effects */}
+      <LoadingScreen />
       <UpdateLog />
+      <FloatingHearts />
+      <TulipEasterEgg />
+      
       <Navbar />
       
-      {/* Page Sections */}
+      {/* Main Content Sections */}
       <div className="flex flex-col w-full">
         <HeroVideo />
+        <RecordHolder />
+        <Foryoupics />
         <BulletinBoard />
         <PolaroidGallery />
         <VoiceSnippets />
         
-        {/* NEW MOVIE SECTION */}
+        {/* New Midnight Matinees Section */}
         <MovieReviews />
         
         <LoveLetters />
         <FishbowlMessages />
+        <VirtualPet />
       </div>
 
       {/* Persistent UI Elements */}
       <FloatingMusicPlayer />
       <CornerCompanions />
+      <ComebackButton />
       
     </main>
   );
