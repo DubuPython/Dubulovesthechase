d683257a87d5301d5e5fa6756b6464681404b6cf
Dubu <jethroberboso25@gmail.com>
2026-09-02 11:48:18 +0800

    added monthsary celebration

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,59 @@
-import Navbar from '../components/Layout/Navbar';
-import HeroVideo from '../components/Sections/HeroVideo';
-import RecordHolder from '../components/Sections/RecordHolder';
-import Foryoupics from '../components/Sections/Foryoupics';
-import BulletinBoard from '../components/Sections/BulletinBoard';
-import PolaroidGallery from '../components/Sections/PolaroidGallery';
-import VoiceSnippets from '../components/Sections/VoiceSnippets';
-import MovieReviews from '../components/Sections/MovieReviews';
-import LoveLetters from '../components/Sections/LoveLetters';
-import FishbowlMessages from '../components/Sections/FishbowlMessages';
-import VirtualPet from '../components/Sections/VirtualPet';
-
-import UpdateLog from '../components/UI/Updatelog';
-import FloatingMusicPlayer from '../components/UI/FloatingMusicPlayer';
-import CornerCompanions from '../components/UI/CornerCompanions';
-import FloatingHearts from '../components/UI/FloatingHearts';
-import LoadingScreen from '../components/UI/LoadingScreen';
-import TulipEasterEgg from '../components/UI/TulipEasterEgg';
-import ComebackButton from '../components/UI/ComebackButton';
+import Navbar from "../components/Layout/Navbar";
+import BulletinBoard from "../components/Sections/BulletinBoard";
+import HeroVideo from "../components/Sections/HeroVideo";
+import PolaroidGallery from "../components/Sections/PolaroidGallery";
+import FishbowlMessages from "../components/Sections/FishbowlMessages"; 
+import RecordHolder from "../components/Sections/RecordHolder";
+import VirtualPet from "../components/Sections/VirtualPet"; 
+import FloatingHearts from "../components/UI/FloatingHearts"; 
+import FloatingMusicPlayer from "../components/UI/FloatingMusicPlayer";
+import LoveLetters from '@/components/Sections/LoveLetters';
+import Comeback from '@/components/UI/ComebackButton';
+import Updates from '@/components/UI/Updatelog';
+import PicturesForYou from '@/components/Sections/foryoupics';
+import LoadingScreen from '@/components/UI/LoadingScreen';
+import CornerCompanions from '@/components/UI/CornerCompanions';
+import TulipEasterEgg from '@/components/UI/TulipEasterEgg';
 
 export default function Home() {
   return (
-    <main className="relative w-full min-h-screen bg-slate-50 dark:bg-[#0f0f1b] overflow-x-hidden selection:bg-pink-300 selection:text-pink-900">
+    <main className="relative min-h-screen font-sans overflow-x-hidden no-scrollbar">
       
-      {/* Loaders and Full-Screen Effects */}
+      {/* 1. The Waddle-and-Slide Loading Screen */}
       <LoadingScreen />
-      <UpdateLog />
-      <FloatingHearts />
-      <TulipEasterEgg />
       
-      <Navbar />
-      
-      {/* Main Content Sections */}
-      <div className="flex flex-col w-full">
-        <HeroVideo />
-        <RecordHolder />
-        <Foryoupics />
-        <BulletinBoard />
-        <PolaroidGallery />
-        <VoiceSnippets />
-        
-        {/* New Midnight Matinees Section */}
-        <MovieReviews />
-        
-        <LoveLetters />
-        <FishbowlMessages />
-        <VirtualPet />
+      <div className="fixed inset-0 z-[1] bg-gradient-to-br from-indigo-100 via-purple-50 to-pink-100 dark:from-[#0f0c29] dark:via-[#302b63] dark:to-[#24243e] transition-colors duration-700" />
+
+      <div className="absolute top-0 left-0 w-full h-full z-[2] pointer-events-none overflow-hidden">
+        <FloatingHearts />
       </div>
 
-      {/* Persistent UI Elements */}
-      <FloatingMusicPlayer />
-      <CornerCompanions />
-      <ComebackButton />
+      <div className="relative z-[10] w-full flex flex-col items-center">
+        <Navbar />
+        <FloatingMusicPlayer />
+        
+        <HeroVideo />
+        <div className="w-full">
+          <BulletinBoard />
+        </div>
+        <LoveLetters />
+        
+        {/* 2. Wrapped the Gallery to position the hidden Tulip Easter Egg */}
+        <div className="relative w-full">
+          <TulipEasterEgg />
+          <PolaroidGallery />
+        </div>
+        
+        <PicturesForYou />
+        <FishbowlMessages />
+        <RecordHolder />
+        <VirtualPet />
+        <Comeback />
+        <Updates />
+        
+        {/* 3. The Quiet Corner Companions */}
+        <CornerCompanions />
+      </div>
       
     </main>
   );
